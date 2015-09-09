909def5d5d0baf910e3a03e9ae01bccd839a6a24
Stas Ukolov <ukoloff@gmail.com>
2015-09-09 16:48:07 +0500

    Add shortcut to SQLIte3 DB

--- adminer/login.php
+++ adminer/login.php
@@ -19,3 +19,5 @@
 <p>
 <input type="hidden" name="auth[driver]" value="pgsql">
 <input type="submit" value="<?= lang('Login') ?>">
+<hr>
+<a href='?sqlite=&amp;username=&amp;db=C%3A%5CMy%5Crepo%5Ckabinet_karat%5Cdb%5Csqlite%5Cdevelopment.sqlite3'>SQLite3</a>
